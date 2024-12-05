f8f5506d515da4022781867fc6977c59fc2be071
tommyu-mg <turdanabia@spmengineering.fr>
2024-12-05 16:45:09 -0300

    Added months in header for half day

--- packages/resource-timeline/src/Header.svelte
+++ packages/resource-timeline/src/Header.svelte
@@ -6,26 +6,37 @@
 
 {#if toSeconds($slotDuration)}
 
-    <div class="{$theme.header}" bind:this={$_headerEl}>
+    <!-- <div class="{$theme.header}" bind:this={$_headerEl}>
         <div class="{$theme.days}" role="row">
-            {#each $_viewDates as date}
-                <div class="{$theme.day} {$theme.weekdays?.[date.getUTCDay()]}">
-                    <div class="{$theme.dayHead}">
-                        <time
-                            datetime="{toISOString(date, 10)}"
-                            aria-label="{$_intlDayHeaderAL.format(date)}"
-                            use:setContent={$_intlDayHeader.format(date)}
-                        ></time>
-                    </div>
-                    <div class="{$theme.times}">
-                        {#each $_dayTimes[date.getTime()] as time}
-                            <div class="{$theme.time}" role="columnheader">
-                                <time
-                                    datetime="{time[0]}"
-                                    use:setContent={time[1]}
-                                ></time>
-                            </div>
-                        {/each}
+            {#each $_viewDates as date} -->
+    <div class="{$theme.header}" bind:this={$_headerEl}>
+        <div class="ec-months"  role="row">
+            {#each Object.entries($_viewMonths) as [key, month]}
+                <div class="ec-month ">
+                    <span class="span-month">{key}</span>
+                    <div class="{$theme.days}" role="row">
+
+                        {#each month as date}
+                            <div class="{$theme.day} {$theme.weekdays?.[date.getUTCDay()]}">
+                                <div class="{$theme.dayHead}">
+                                    <time
+                                        datetime="{toISOString(date, 10)}"
+                                        aria-label="{$_intlDayHeaderAL.format(date)}"
+                                        use:setContent={$_intlDayHeader.format(date)}
+                                    ></time>
+                                </div>
+                                <div class="{$theme.times}">
+                                    {#each $_dayTimes[date.getTime()] as time}
+                                        <div class="{$theme.time}" role="columnheader">
+                                            <time
+                                                datetime="{time[0]}"
+                                                use:setContent={time[1]}
+                                            ></time>
+                                        </div>
+                                    {/each}
+                                </div>
+                            </div>   
+                        {/each} 
                     </div>
                 </div>
                 {/each}
